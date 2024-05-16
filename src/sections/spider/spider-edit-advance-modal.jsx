1e8fab15dfce2377ada382e5fb46f2aaff3cbdff
NguyenDuyTheDev <125572099+NguyenDuyTheDev@users.noreply.github.com>
2024-05-16 21:58:48 +0700

    fix layout

--- src/sections/spider/spider-edit-advance-modal.jsx
+++ src/sections/spider/spider-edit-advance-modal.jsx
@@ -26,11 +26,12 @@ const style = {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: "70%",
-    height: "100%",
+    height: "75%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
+    overflow: "auto",
 };
 
 const ITEM_HEIGHT = 48;
